6848186f686ef45ee7ebc9c0d6ff4d2f36aea069
Akashdeep Singh <johnkutabou@gmail.com>
2023-04-04 18:40:51 +0530

    Add initial commit

---
+++ src/Index.java
@@ -0,0 +1,10 @@
+
+public class Index {
+	
+	public static void main(String[] args){
+	    System.out.println("Welcome to Music Library");
+		System.out.println("Would you like to:\n 1. Open a playlist \n 2. Create s playlist \n 3. Browse playlist ");
+			
+	}
+	  
+}
